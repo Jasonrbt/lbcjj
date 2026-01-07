9b302a2a72835045ba229fd048c44e97d9a22a89
Jason <Narberal766@gmail.com>
2026-01-07 10:00:59 +0100

    Ajout du gabarit.php, modif index.php, modif controler et correction createUser.php

--- Controller/controlUser.php
+++ Controller/controlUser.php
@@ -1,46 +1,25 @@
 <?php
     require_once __DIR__ . '/../Modele/modele.php';
 
-    $action = $_GET['action'] ?? 'form';
-
-    switch ($action) {
-        case 'list':
-            $users = getUsers();
-            $content = __DIR__ . '/../Vue/listeUser.php';
-            break;
-
-        case 'form':
-            $content = __DIR__ . '/../Vue/createUser.php';
-            break;
-            
-            
-        case 'create' :
-            create();
-            break;
-            
-        default: require __DIR__ .  'index.php';
-        break;
-    }
-
     function create() {
 //echo "Controller appelé";
 //die();
         if (empty($_POST['nom_user']) || empty($_POST['prenom_user']) || empty($_POST['mdp_user']) || empty($_POST['mail_user']) ) {   
             $error = "Tous les champs sont obligatoires.";
-            $content = __DIR__ . '/../Vue/createUser.php';
-            require $content;
+            $content = 'Vue/createUser.php';
+            require __DIR__ . '/../Vue/gabarit.php';
             return;
         }
         if (strlen($_POST['mdp_user']) < 5) {
             $error = "Le mot de passe doit contenir au moins 5 caractères.";
-            $content = __DIR__ . '/../Vue/createUser.php';
-            require 'index.php';
+            $content = 'Vue/createUser.php';
+            require __DIR__ . '/../Vue/gabarit.php';
             exit;
         }
         if (getUserByEmail($_POST['mail_user'])) {
             $error = "Un utilisateur avec cet email existe déjà.";
-            $content = __DIR__ . '/../Vue/createUser.php';
-            require 'index.php';
+            $content = 'Vue/createUser.php';
+            require __DIR__ . '/../Vue/gabarit.php';
             exit;
         }
         createUser($_POST['nom_user'], $_POST['prenom_user'], $_POST['mdp_user'], $_POST['mail_user']);    
--- Vue/createUser.php
+++ Vue/createUser.php
@@ -1,36 +1,25 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <title>Créer un utilisateur</title>
-    </head>
-    <body>
+<h1>Créer un utilisateur</h1>
 
-        <h1>Créer un utilisateur</h1>
+<?php if (!empty($error)) : ?>
+    <p style="color:red"><?= htmlspecialchars($error) ?></p>
+<?php endif; ?>
 
-        <?php if (!empty($error)) : ?>
-            <p style="color:red"><?= htmlspecialchars($error) ?></p>
-        <?php endif; ?>
+<?php if (!empty($success)) : ?>
+    <p><?= htmlspecialchars($success) ?></p>
+<?php endif; ?>
 
-        <?php if (!empty($success)) : ?>
-            <p><?= htmlspecialchars($success) ?></p>
-        <?php endif; ?>
+<form method="POST" action="index.php?action=create">
+    <label>Nom</label><br>
+    <input type="text" name="nom_user" required><br><br>
 
-        <form method="POST" action="index.php?action=create">
-            <label>Nom</label><br>
-            <input type="text" name="nom_user" required><br><br>
+    <label>Prénom</label><br>
+    <input type="text" name="prenom_user" required><br><br>
 
-            <label>Prénom</label><br>
-            <input type="text" name="prenom_user" required><br><br>
+    <label>Email</label><br>
+    <input type="email" name="mail_user" required><br><br>
 
-            <label>Email</label><br>
-            <input type="email" name="mail_user" required><br><br>
+    <label>Mot de passe</label><br>
+    <input type="password" name="mdp_user" required><br><br>
 
-            <label>Mot de passe</label><br>
-            <input type="password" name="mdp_user" required><br><br>
-
-            <button type="submit">Créer</button>
-        </form>
-
-    </body>
-</html>
+    <button type="submit">Créer</button>
+</form>
