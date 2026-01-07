2d95aa6e24a07641ecc730d7f3f35045ae699467
GohanSenior <jerome.gatti@hotmail.fr>
2026-01-07 13:30:10 +0100

    fusion des 2 controleurs en 1 seul, ajout du style, correction du chemin dans index.php

--- Modele/Modele.php
+++ Modele/Modele.php
@@ -1,128 +1,129 @@
-<?php 
-    function getBdd()
-    {
-        $bdd = new PDO(
-            'mysql:host=localhost:3306;dbname=lbcjj;charset=utf8',
-            'root', '',
-            array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
-        );
-        return $bdd;
-    }
+<?php
+function getBdd()
+{
+    $bdd = new PDO(
+        'mysql:host=localhost:3306;dbname=lbcjj;charset=utf8',
+        'root',
+        '',
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+    );
+    return $bdd;
+}
 
-    function getUsers() 
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('SELECT * FROM compte_utilisateur ORDER BY id_user');
-        $req->execute();
-        $users = $req->fetchAll(PDO::FETCH_ASSOC);
-        return $users;
-    }
+function getUsers()
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('SELECT * FROM compte_utilisateur ORDER BY id_user');
+    $req->execute();
+    $users = $req->fetchAll(PDO::FETCH_ASSOC);
+    return $users;
+}
 
-    function getUserByEmail($email)
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('SELECT * FROM compte_utilisateur WHERE mail_user = ?');
-        $req->execute([$email]);
-        $user = $req->fetch(PDO::FETCH_ASSOC);
-        return $user;
-    }
-    
-    function createUser($nom_user, $prenom_user, $mdp_user, $mail_user)
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('INSERT INTO compte_utilisateur (nom_user, prenom_user, mdp_user, mail_user) VALUES (?, ?, ?, ?)');
-        $req->execute(array($nom_user, $prenom_user, $mdp_user, $mail_user));
-    }
+function getUserByEmail($email)
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('SELECT * FROM compte_utilisateur WHERE mail_user = ?');
+    $req->execute([$email]);
+    $user = $req->fetch(PDO::FETCH_ASSOC);
+    return $user;
+}
 
-    function deleteUser($id_user)
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('DELETE FROM compte_utilisateur WHERE id_user = ?');
-        $req->execute(array($id_user));
-    }
+function createUser($nom_user, $prenom_user, $mdp_user, $mail_user)
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('INSERT INTO compte_utilisateur (nom_user, prenom_user, mdp_user, mail_user) VALUES (?, ?, ?, ?)');
+    $req->execute(array($nom_user, $prenom_user, $mdp_user, $mail_user));
+}
 
-    function updateUser($id_user, $nom_user, $prenom_user, $mdp_user, $mail_user)
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('UPDATE compte_utilisateur SET nom_user = ?, prenom_user = ?, mdp_user = ?, mail_user = ? WHERE id_user = ?');
-        $req->execute(array($nom_user, $prenom_user, $mdp_user, $mail_user, $id_user));
-    }
+function deleteUser($id_user)
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('DELETE FROM compte_utilisateur WHERE id_user = ?');
+    $req->execute(array($id_user));
+}
 
-    // ===== Fonctions pour les annonces =====
+function updateUser($id_user, $nom_user, $prenom_user, $mdp_user, $mail_user)
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('UPDATE compte_utilisateur SET nom_user = ?, prenom_user = ?, mdp_user = ?, mail_user = ? WHERE id_user = ?');
+    $req->execute(array($nom_user, $prenom_user, $mdp_user, $mail_user, $id_user));
+}
 
-    function getAllAnnonces()
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('SELECT * FROM annonce ORDER BY DATE_ DESC');
-        $req->execute();
-        return $req->fetchAll(PDO::FETCH_ASSOC);
-    }
+// ===== Fonctions pour les annonces =====
 
-    function getAnnoncesByName($name)
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('SELECT * FROM annonce WHERE TITRE_ANNONCE LIKE ? ORDER BY DATE_ DESC');
-        $req->execute(array('%' . $name . '%'));
-        return $req->fetchAll(PDO::FETCH_ASSOC);
-    }
+function getAllAnnonces()
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('SELECT * FROM annonce ORDER BY DATE_ DESC');
+    $req->execute();
+    return $req->fetchAll(PDO::FETCH_ASSOC);
+}
 
-    function getAnnonceById($id_annonce)
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('SELECT * FROM annonce WHERE ID_ANNONCE = ?');
-        $req->execute(array($id_annonce));
-        return $req->fetch(PDO::FETCH_ASSOC);
-    }
+function getAnnoncesByName($name)
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('SELECT * FROM annonce WHERE TITRE_ANNONCE LIKE ? ORDER BY DATE_ DESC');
+    $req->execute(array('%' . $name . '%'));
+    return $req->fetchAll(PDO::FETCH_ASSOC);
+}
 
-    function addAnnonce($titre, $description, $prix, $id_user)
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('INSERT INTO annonce (TITRE_ANNONCE, DESCRIPTION, PRIX, DATE_, ID_USER) VALUES (?, ?, ?, NOW(), ?)');
-        $req->execute(array($titre, $description, $prix, $id_user));
-        return $bdd->lastInsertId();
-    }
+function getAnnonceById($id_annonce)
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('SELECT * FROM annonce WHERE ID_ANNONCE = ?');
+    $req->execute(array($id_annonce));
+    return $req->fetch(PDO::FETCH_ASSOC);
+}
 
-    function deleteAnnonce($id_annonce)
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('DELETE FROM annonce WHERE ID_ANNONCE = ?');
-        $req->execute(array($id_annonce));
-    }
+function addAnnonce($titre, $description, $prix, $id_user)
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('INSERT INTO annonce (TITRE_ANNONCE, DESCRIPTION, PRIX, DATE_, ID_USER) VALUES (?, ?, ?, NOW(), ?)');
+    $req->execute(array($titre, $description, $prix, $id_user));
+    return $bdd->lastInsertId();
+}
 
-    function updateAnnonce($id_annonce, $titre, $description, $prix)
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('UPDATE annonce SET TITRE_ANNONCE = ?, DESCRIPTION = ?, PRIX = ? WHERE ID_ANNONCE = ?');
-        $req->execute(array($titre, $description, $prix, $id_annonce));
-    }
+function deleteAnnonce($id_annonce)
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('DELETE FROM annonce WHERE ID_ANNONCE = ?');
+    $req->execute(array($id_annonce));
+}
 
-    // ===== Fonctions pour les images =====
+function updateAnnonce($id_annonce, $titre, $description, $prix)
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('UPDATE annonce SET TITRE_ANNONCE = ?, DESCRIPTION = ?, PRIX = ? WHERE ID_ANNONCE = ?');
+    $req->execute(array($titre, $description, $prix, $id_annonce));
+}
 
-    function getImagesByAnnonceId($id_annonce)
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('SELECT * FROM image WHERE ID_ANNONCE = ?');
-        $req->execute(array($id_annonce));
-        return $req->fetchAll(PDO::FETCH_ASSOC);
-    }
+// ===== Fonctions pour les images =====
 
-    function addImage($id_annonce, $nomFichier, $ordre)
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('INSERT INTO image (ID_ANNONCE, chemin_image, ordre) VALUES (?, ?, ?)');
-        $req->execute(array($id_annonce, $nomFichier, $ordre));
-    }
+function getImagesByAnnonceId($id_annonce)
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('SELECT * FROM image WHERE ID_ANNONCE = ?');
+    $req->execute(array($id_annonce));
+    return $req->fetchAll(PDO::FETCH_ASSOC);
+}
 
-    function deleteImagesByAnnonceId($id_annonce)
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('DELETE FROM image WHERE ID_ANNONCE = ?');
-        $req->execute(array($id_annonce));
-    }
+function addImage($id_annonce, $nomFichier, $ordre)
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('INSERT INTO image (ID_ANNONCE, chemin_image, ordre) VALUES (?, ?, ?)');
+    $req->execute(array($id_annonce, $nomFichier, $ordre));
+}
 
-    function deleteImageById($id_image)
-    {
-        $bdd = getBdd();
-        $req = $bdd->prepare('DELETE FROM image WHERE ID_IMAGE = ?');
-        $req->execute(array($id_image));
-    }
+function deleteImagesByAnnonceId($id_annonce)
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('DELETE FROM image WHERE ID_ANNONCE = ?');
+    $req->execute(array($id_annonce));
+}
+
+function deleteImageById($id_image)
+{
+    $bdd = getBdd();
+    $req = $bdd->prepare('DELETE FROM image WHERE ID_IMAGE = ?');
+    $req->execute(array($id_image));
+}
--- index.php
+++ index.php
@@ -1,52 +1,50 @@
-<?php 
-    require_once 'Controller/controlUser.php';
-             
-    session_start();
+<?php
+require_once 'Controleur/Controleur.php';
 
-    $action = $_GET['action'] ?? 'home';
+session_start();
 
-    switch ($action) {
-        case 'list':
-            $users = getUsers();
-            $content = 'Vue/listeUser.php';
-            break;
-        case 'form':
-            $content = 'Vue/createUser.php';
-            break;   
-        case 'create' :
-            create();
-            break;
-        case 'login' :
-            $content = 'Vue/loginUser.php';
-            break;
-        case 'createAnnonce':
-            require "Controleur/Controleur.php";
-            createAnnonceController();
-            break;       
-        case 'formAnnonce':
-            require "Vue/annonce_form.php";
-            break;
-        case 'voirAnnonce':
-            require "Controleur/Controleur.php";
-            voirAnnonceController();
-            break;
-        case 'editAnnonce':
-            require "Controleur/Controleur.php";
-            editAnnonceController();
-            break;
-        case 'updateAnnonce':
-            require "Controleur/Controleur.php";
-            updateAnnonceController();
-            break;
-        case 'deleteAnnonce':
-            require "Controleur/Controleur.php";
-            deleteAnnonceController();
-            break; 
-        default: 
-            $content = null;
-            break;
-      }
-    
-    require_once 'Vue/gabarit.php';
-?> 
+$action = $_GET['action'] ?? 'home';
 
+switch ($action) {
+    case 'list':
+        $users = getUsers();
+        $content = 'Vue/listeUser.php';
+        break;
+    case 'form':
+        $content = 'Vue/createUser.php';
+        break;
+    case 'create':
+        create();
+        break;
+    case 'login':
+        $content = 'Vue/loginUser.php';
+        break;
+    case 'createAnnonce':
+        require "Controleur/Controleur.php";
+        createAnnonceController();
+        break;
+    case 'formAnnonce':
+        require "Vue/annonce_form.php";
+        break;
+    case 'voirAnnonce':
+        require "Controleur/Controleur.php";
+        voirAnnonceController();
+        break;
+    case 'editAnnonce':
+        require "Controleur/Controleur.php";
+        editAnnonceController();
+        break;
+    case 'updateAnnonce':
+        require "Controleur/Controleur.php";
+        updateAnnonceController();
+        break;
+    case 'deleteAnnonce':
+        require "Controleur/Controleur.php";
+        deleteAnnonceController();
+        break;
+    default:
+        $content = null;
+        break;
+}
+
+require_once 'Vue/gabarit.php';
